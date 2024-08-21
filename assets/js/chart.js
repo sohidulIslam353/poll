486e889f725db36f78f81170cb037c3c3a34dc90
sohidulislam353 <sohidulislam353@gmail.com>
2024-08-21 23:57:19 +0600

    changes

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -27,32 +27,22 @@ const barChart = new Chart(document.getElementById('barChart'), {
 });
 
 // Pie Chart Configuration
+const countryWiseCount = [80, 20];
+const locationName = ['Bangladesh', 'Overseas']; // Example country names
 const pieChart = new Chart(document.getElementById('pieChart'), {
     type: 'pie',
     data: {
-        labels: countryNames,
+        labels: locationName,
         datasets: [{
             label: 'Vote Distribution',
-            data: voteCounts,
+            data: countryWiseCount,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(54, 162, 235, 0.6)',
-                'rgba(255, 206, 86, 0.6)',
-                'rgba(75, 192, 192, 0.6)',
-                'rgba(153, 102, 255, 0.6)',
-                'rgba(153, 102, 255, 0.6)',
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(255, 206, 86, 0.6)',
+                'green',
+                'orange',
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 206, 86, 0.6)',
             ],
             borderWidth: 1
         }]
